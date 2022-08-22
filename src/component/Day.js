a40c54c33ad3c2c13ccb05c5c8295002f87b7dc7
sys8517 <sys8517@naver.com>
2022-08-22 15:39:02 +0900

    version 1.0

---
+++ src/component/Day.js
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import useFetch from "../hooks/useFetch";
+import Word from "./Word";
+export default function Day() {
+  const { day } = useParams();
+//   const day = useParams().day;
+  //   const wordList = dummy.words.filter((word) => word.day === Number(day));
+
+//   const [words, setWords] = useState([]);
+
+//   useEffect(() => {
+//     fetch(`http://localhost:3001/words?day=${day}`)
+//   .then(res => {
+//       return res.json();
+//   })
+//   .then(data => {
+//       setWords(data);
+//   })
+// },[day])
+
+const words = useFetch(`http://localhost:3001/words?day=${day}`)
+
+  return (
+    <>
+      <h2>Day {day}</h2>
+      <table>
+        <tbody>
+          {words.map((word) => (
+            <Word word={word} key={word.id} />
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+}
---
+++ src/component/DayList.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import useFetch from "../hooks/useFetch";
+
+export default function DayList() {
+    const days = useFetch("http://localhost:3001/days"); //custom hook 사용
+
+//   const [days, setDays] = useState([]);
+
+//   useEffect(() => {
+//     fetch('http://localhost:3001/days')
+//     .then(res => {
+//         return res.json();
+//     })
+//     .then(data => {
+//         setDays(data);
+//     })
+//   }, []); //렌더링 후 한번만 실행하기 위해 빈 배열을 넣어준다.
+
+  return (
+    <ul className="list_day">
+      {days.map((day) => (
+        <li key={day.id}>
+          <Link to={`/day/${day.day}`}>Day {day.day}</Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
